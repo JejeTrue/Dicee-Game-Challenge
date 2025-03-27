4375277b8fe2ad24b50f6b4c19b25490157de2da
JejeTrue <jejedanjihaan@gmail.com>
2025-03-27 23:55:55 +0700

    Step 4: Change the <img> to a Random Dice

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // random number between 1 and 6
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// pick out a random dice image between dice1.png to dice 6.png then place this image inside the left <img> element
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
