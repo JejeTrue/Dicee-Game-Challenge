daee2b59d853bd5bcfc57e9646c6caf93fd59c54
JejeTrue <jejedanjihaan@gmail.com>
2025-03-28 00:01:47 +0700

    Step 6: change the Title to Display a Winner

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+"
 // for img2
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+
+// title will change based on the result of the dice roll
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+}else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
